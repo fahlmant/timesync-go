9f80a91258be7c342a6f1957e8e203e8ed4443d7
Taylor <fahlmantaylor@gmail.com>
2016-11-22 10:02:26 -0800

    init commit with basic go web app

---
+++ src/app.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
