64f830be949676252719f04e87f6ddd51bbc43c7
fahlmant <fahlmantaylor@gmail.com>
2017-01-06 12:24:56 -0800

    More comments

--- src/app.go
+++ src/app.go
@@ -11,6 +11,7 @@ import (
 
 
 func main() {
+    //Create http handler
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
