b5be87039fa18493d69f92c1d2a77374a306c08b
fahlmant <fahlmantaylor@gmail.com>
2017-01-06 12:50:06 -0800

    Comment test

--- src/app.go
+++ src/app.go
@@ -9,12 +9,11 @@ import (
     "net/http"
 )
 
-
 func main() {
     //Create http handler
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
-
+    //Bind to 8080
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
